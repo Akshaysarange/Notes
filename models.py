93fe8f87371791d44253cd42a81c62c961c761c0
Akshay Sarange <akshaysarange8@gmail.com>
2026-07-20 17:44:53 +0530

    database model

---
+++ models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+print(db)
+print(Note)
